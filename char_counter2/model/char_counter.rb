229d14cc79ee6f6c2a03bf1c69b37d70d6058082
javierperini <javierperini90@gmail.com>
2015-04-16 15:54:57 -0300

    Sexto test funcionando

--- char_counter2/model/char_counter.rb
+++ char_counter2/model/char_counter.rb
@@ -1,9 +1,11 @@
 class CharCounter
 
   def self.count(key)
-    dic = Hash.new()
-    keys= key.delete(" ")
-    keys.each_char{|char| dic[char]= key.count(char) }
+    dic = Hash.new
+    if !key.nil?
+      keys= key.delete(" ")
+      keys.each_char{|char| dic[char]= key.count(char) }
+    end
     dic
   end
 
