196e6e994010b0722acf8326e30360a74d84dc88
javierperini <javierperini90@gmail.com>
2015-04-16 15:47:53 -0300

    Cuarto test funcionando

--- char_counter2/model/char_counter.rb
+++ char_counter2/model/char_counter.rb
@@ -2,7 +2,8 @@ class CharCounter
 
   def self.count(key)
     dic = Hash.new()
-    key.each_char{|char| dic[char]= key.count(char) }
+    keys= key.delete(" ")
+    keys.each_char{|char| dic[char]= key.count(char) }
     dic
   end
 
