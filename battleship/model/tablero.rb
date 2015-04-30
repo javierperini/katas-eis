952a161b32e27b050adcc7b1103a91548341955d
javierperini <javierperini90@gmail.com>
2015-04-30 16:53:43 -0300

    #Test 1 incializando el tablero y compruebo su coordenada de ancho

---
+++ battleship/model/tablero.rb
@@ -0,0 +1,11 @@
+class Tablero
+  def initialize(ancho, alto)
+    @ancho= ancho
+    @alto= alto
+  end
+
+  def get_ancho
+    @ancho
+  end
+
+end
---
+++ battleship/spec/tablero_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require_relative '../model/tablero.rb'
+
+describe 'Tablero' do
+  before do
+    @tablero= Tablero.new(5,5)
+  end
+
+  it 'compruebo que el ancho del tablero es 5' do
+    expect(@tablero.get_ancho).to eq 5
+  end
+
+end
