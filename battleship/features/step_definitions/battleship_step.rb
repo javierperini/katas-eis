c4abc5ccad969c0acd8f97943e66c8d681a7cd0f
javierperini <javierperini90@gmail.com>
2015-05-02 15:24:58 -0300

    #Scenario 1  crea un barco chico con posicion valida y se comprueba que esa posicion esta ocupada

--- battleship/features/step_definitions/battleship_step.rb
+++ battleship/features/step_definitions/battleship_step.rb
@@ -1,16 +1,17 @@
 require_relative '../../model/tablero.rb'
+require_relative '../../model/columna.rb'
+require_relative '../../model/fila.rb'
 
 Given(/^a board with dimensions "([^"]*)" x "([^"]*)"$/) do |xDimension, yDimension|
-  @tablero= Tablero.new(xDimension,yDimension)
+  @tablero= Tablero.new(xDimension.to_i, yDimension.to_i)
 end
 
-Given(/^I create a small ship in position "([^"]*)"$/) do |posicionX ,posicionY|
-  @tablero.crear_barco_chico(posicionX, posicionY)
+Given(/^I create a small ship in position "(\d+):(\d+)"$/) do |posicionX ,posicionY|
+  @tablero.crear_barco_chico(posicionX.to_i, posicionY.to_i)
 end
 
-
-Then(/^position "([^"]*)" is not empty$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+Then(/^position "(\d+):(\d+)" is not empty$/) do |posicionX, posicionY|
+  expect(@tablero.posicion_ocupada?(posicionX.to_i,posicionY.to_i)).to eq(true)
 end
 
 
