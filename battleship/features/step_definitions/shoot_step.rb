11c2fbae271ec7b1f45f51175bfb28fc3397aac3
javierperini <javierperini90@gmail.com>
2015-05-02 16:36:11 -0300

    # Scenario 3 disparo a una posicion y obtengo un hit

--- battleship/features/step_definitions/shoot_step.rb
+++ battleship/features/step_definitions/shoot_step.rb
@@ -7,11 +7,11 @@ Given(/^a large ship in position: “(\d+):(\d+)”$/) do |posicionX, posicionY|
 end
 
 Given(/^I shoot to position “(\d+):(\d+)”$/) do |posicionX, posicionY|
-  @tablero.disparar_posicion(posicionX,posicionY)
+  @tablero.disparar_posicion(posicionX.to_i,posicionY.to_i)
 end
 
 Then(/^I get hit$/) do
-  expect(@tablero.get_hit()).to eq 1
+  expect(@tablero.es_hit).to eq 1
 end
 
 
