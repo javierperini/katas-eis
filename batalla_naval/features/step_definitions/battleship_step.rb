1419df1bdbfa9591f745d03009930f270e11aabd
javierperini <javierperini90@gmail.com>
2015-05-04 14:32:55 -0300

    #Scenario 2  Creo un barco grande y compruebo que su posicion es valida

--- batalla_naval/features/step_definitions/battleship_step.rb
+++ batalla_naval/features/step_definitions/battleship_step.rb
@@ -4,7 +4,6 @@ require_relative '../../model/fila.rb'
 require_relative '../../model/barco.rb'
 
 
-
 Given(/^a board with dimensions "([^"]*)" x "([^"]*)"$/) do |posicionX, posicionY|
   @tablero= Tablero.new(posicionX.to_i, posicionY.to_i)
 end
@@ -14,7 +13,7 @@ Given(/^I create a small ship in position "(\d+):(\d+)"$/) do |posicionX ,posici
 end
 
 Given(/^I create a large ship in position "(\d+):(\d+)"$/) do |posicionX ,posicionY|
-  pending # express the regexp above with the code you wish you had
+  @tablero.crear_barco_grande(posicionX.to_i, posicionY.to_i)
 end
 
 Then(/^position "(\d+):(\d+)" is not empty$/) do |posicionX, posicionY|
