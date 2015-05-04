e3f43afa6d151a6adf342b2e11916cf2a83b1279
javierperini <javierperini90@gmail.com>
2015-05-04 14:34:08 -0300

    #Scenario 2  creo un barco grande y compruebo que su poscion es valida

--- batalla_naval/features/step_definitions/battleship_step.rb
+++ batalla_naval/features/step_definitions/battleship_step.rb
@@ -12,7 +12,7 @@ Given(/^I create a small ship in position "(\d+):(\d+)"$/) do |posicionX ,posici
   @tablero.crear_barco_chico(posicionX.to_i, posicionY.to_i)
 end
 
-Given(/^I create a large ship in position "(\d+):(\d+)"$/) do |posicionX ,posicionY|
+Given(/^I create a large ship in position "(\d+):(\d+)"$/) do | posicionX ,posicionY|
   @tablero.crear_barco_grande(posicionX.to_i, posicionY.to_i)
 end
 
