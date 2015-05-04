c81068a7b7da1e82104d290b802bb9fece694eba
javierperini <javierperini90@gmail.com>
2015-05-04 15:05:24 -0300

    #Scenario 3 disparo y retorno  un hit

--- batalla_naval/features/step_definitions/shoot_step.rb
+++ batalla_naval/features/step_definitions/shoot_step.rb
@@ -1,4 +1,13 @@
+require_relative '../../model/tablero.rb'
+require_relative '../../model/columna.rb'
+require_relative '../../model/fila.rb'
+require_relative '../../model/barco.rb'
+require_relative '../../model/disparo.rb'
 
-Given(/^a large ship in position: “(\d+):(\d+)”$/) do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+Given(/^a large ship in position: “(\d+):(\d+)”$/) do |posicionX, posicionY|
+  @tablero.crear_barco_grande(posicionX.to_i, posicionY.to_i)
+end
+
+Given(/^I shoot to position “(\d+):(\d+)”$/) do |posicionX, posicionY|
+  @tablero.disparar_posicion(posicionX.to_i,posicionY.to_i)
 end
